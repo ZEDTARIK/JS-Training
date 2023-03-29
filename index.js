f6e5a5d16a71badb5197a9fedabad5bf80e0b5fd
zouhair.ettarak <ettarak.zouhair@gmail.com>
2023-03-29 22:54:20 +0000

    boucle for break and continue

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-// split
+const array = [1, 6, 10, 500, 4];
 
-var str = "Zouhair ettarak";
-let split = str.split('');
-console.table(split);
+for (let index = 0; index < array.length; index++) {
+    
+    if(array[index] === 500)  break; 
+    //if(array[index] === 500)  continue; 
+    array[index] = array[index]  - 1;
+}
+
+console.table(array);
