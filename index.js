907ad38672c525f4156e26ee4e1134fa0d527080
zouhair.ettarak <ettarak.zouhair@gmail.com>
2023-03-29 22:48:10 +0000

    split

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-const doublons = (array) => {
-    let result= [];
-    for (let index = 0; index < array.length; index++) {
-        result.push(array[index] * array[index]);
-    }
-    return result;
-}
+// split
 
-console.table(doublons([1, 2, 4, 5]));
+var str = "Zouhair ettarak";
+let split = str.split('');
+console.table(split);
